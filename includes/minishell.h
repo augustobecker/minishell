5c74d2b0b40b42ac55f447d3d9e3966f5a969d6b
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-10-03 15:35:51 -0300

    [Header] Include para a limits.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 19:17:28 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/10/01 15:09:15 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/10/03 20:35:00 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 
 # include "libft.h"
 # include "types.h"
+# include "limits.h"
 # include <readline/readline.h>
 # include <readline/history.h>
 
-void	prompt(void);
-t_bool	is_prompt_valid(char *prompt);
-char	*pwd(void);
+void	  prompt(void);
+t_bool  is_prompt_valid(char *prompt);
+char    *pwd(void);
 
 #endif
