9af798ffc8f95dec482a46c6ee53e586aaf203ce
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-10-03 15:34:02 -0300

    [Builtin] pwd v1.1 - Tratamento para NULL
    Adicionei a macro PATH_MAX da limits.h que nos permite alocar o maior nome de arquivo permitida pela biblioteca.
    Mais seguro e bem mais legível.
    Além disso coloquei uma condição de retorno caso a cwd falhe.
    O tratamento do erro em si será feito fora da função.

--- sources/builtins.c
+++ sources/builtins.c
@@ -6,7 +6,7 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 15:08:56 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/10/01 15:44:57 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/10/03 20:28:50 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,15 @@ char *pwd(void)
 {
 	char	*pwd;
 
-	pwd = ft_calloc(1, 100);
-	getcwd(pwd, 100);
-	ft_printf(PURPLE);
-	pwd = ft_strappend(&pwd, ">");
-	pwd = ft_strappend(&pwd, " ");
-	pwd = ft_strappend(&pwd, RESET);
-	return (pwd);
+	pwd = ft_calloc(sizeof(char), PATH_MAX);
+	if (getcwd(pwd, PATH_MAX) == NULL)
+		return (NULL);
+	else
+	{
+		ft_printf(PURPLE);
+		pwd = ft_strappend(&pwd, ">");
+		pwd = ft_strappend(&pwd, " ");
+		pwd = ft_strappend(&pwd, RESET);
+		return (pwd);
+	}
 }
