ebb0671ff5690ec6dbeeb161f1e35cfda528556c
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-10-04 09:44:05 -0300

    [Builtin] pwd v2.1 - Redirect do Output
    Muda o protótipo da função, retornando um int para entender se o comando funcionou ou não.
    A pwd não precisa de argumentos  para funcionar, então só coloquei um redirect para o output - que pode ser um arquivo, ou um pipe para outro comando.
    E só após o redirect ele printa o path e retorna 0 indicando sucesso.

--- sources/builtins.c
+++ sources/builtins.c
@@ -6,25 +6,24 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 15:08:56 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/10/03 20:28:50 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/10/04 14:37:56 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char *pwd(void)
+int	pwd(int fd_out)
 {
 	char	*pwd;
 
 	pwd = ft_calloc(sizeof(char), PATH_MAX);
 	if (getcwd(pwd, PATH_MAX) == NULL)
-		return (NULL);
+		return (1);
 	else
 	{
-		ft_printf(PURPLE);
-		pwd = ft_strappend(&pwd, ">");
-		pwd = ft_strappend(&pwd, " ");
-		pwd = ft_strappend(&pwd, RESET);
-		return (pwd);
+		if (fd_out != STDOUT_FILENO)
+			dup2(fd_out, STDOUT_FILENO);
+		ft_printf("%s", pwd);
+		return (0);
 	}
 }
