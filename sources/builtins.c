8f8604344af53e5846c4a31f432e87c8e9a284b5
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-10-04 11:34:04 -0300

    [Builtin] cd v1.0

--- sources/builtins.c
+++ sources/builtins.c
@@ -6,11 +6,14 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 15:08:56 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/10/04 14:37:56 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/10/04 16:08:00 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	pwd(int fd_out);
+int	cd(char *directory);
 
 int	pwd(int fd_out)
 {
@@ -26,4 +29,17 @@ int	pwd(int fd_out)
 		ft_printf("%s", pwd);
 		return (0);
 	}
+}
+
+int	cd(char *directory)
+{
+	if (directory == NULL || directory[0] == '\0')
+		return (chdir("/home"));
+	if (chdir(directory))
+	{
+		ft_printf(GREY"minishell: cd: %s: No such file or directory\n"RESET, directory);
+		return (1);
+	}
+	else
+		return (0);
 }
