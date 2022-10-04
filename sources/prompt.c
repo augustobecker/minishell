121e890506055c1a34e7860e506af338a2655e4e
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-10-04 09:23:09 -0300

    [Prompt] - função que retorna o atual path
    A antiga função pwd foi renomeada para current_path.
    Mesmo com as semelhanças entre ela e a pwd tive que fazer uma separação.
    Porque a pwd funciona como um comando - caso não funcione ele não falha logo de cara, o objetivo dela é simplesmente printar o path.
    Enquanto a current_path tem como objetivo pegar o path e juntar com alguns chars para ficar mais estético, não o printa diretamente na tela e o caso de erro é menos elaborado.

--- sources/prompt.c
+++ sources/prompt.c
@@ -6,20 +6,40 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 19:26:52 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/10/01 15:08:04 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/10/04 14:16:54 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	prompt(void);
+char	*current_path(void);
+
 void	prompt(void)
 {
 	char	*prompt;
 
-	prompt = readline(pwd());
+	prompt = readline(current_path());
 	if (is_prompt_valid(prompt) == false)
 		exit(EXIT_FAILURE);
 	if (ft_strlen(prompt) > 0)
 		add_history(prompt);
 	ft_printf(CYAN"prompt> %s\n"RESET, prompt);
+}
+
+char	*current_path(void)
+{
+	char	*path;
+
+	path = ft_calloc(sizeof(char), PATH_MAX);
+	if (getcwd(path, PATH_MAX) == NULL)
+		return (NULL);
+	else
+	{
+		ft_printf(PURPLE);
+		path = ft_strappend(&path, ">");
+		path = ft_strappend(&path, " ");
+		path = ft_strappend(&path, RESET);
+		return (path);
+	}
 }
