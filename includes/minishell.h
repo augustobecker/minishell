75ae450ec202786590b1c232f2928d5934f34f5a
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-10-19 01:10:45 -0300

    [Header][Syntax] Novos protótipos adicionados
    * Adiciona o .h defines.h recém criado.
    * Corrige um include errado - "limits.h" para <limits.h>.
    * Adiciona o protótipo de 4 novas funções ligadas às validações iniciais (se há de fato algum argumento e sintaxe).

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 19:17:28 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/10/04 15:30:15 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/10/19 05:44:49 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 # include "libft.h"
 # include "types.h"
-# include "limits.h"
+# include "defines.h"
+# include <limits.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 
@@ -24,5 +25,9 @@ t_bool		is_prompt_valid(char *prompt);
 char		*current_path(void);
 int		pwd(int fd_out);
 int		cd(char *directory);
+t_bool		is_syntax_valid(char *expression);
+t_bool		there_is_a_valid_char(char *str, char *limiter);
+t_bool		is_a_limiter(char c);
+int		error(char *message, int exit_code);
 
 #endif
