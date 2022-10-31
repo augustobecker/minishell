c4f593aeb99aa267da43263d60c08f255f05de4a
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-10-31 16:22:44 -0300

    [Core] Tokenization
    Para poder fazer um tratamento dos dados evitando tudo que esteja entre aspas.
    Como checar sintaxe, por exemplo.
    Além disso, é essencial para poder separar os comandos por um pipe - sem que aja qualquer erro caso tenha um pipe entre aspas.
    A função funciona utilizando uma stack para verificar se o conteúdo está entre aspas ou não.
    Caso a stack esteja vazia todas as aspas estão fechadas.
    Caso uma aspa seja aberta, ela é adicionada à stack.
    Caso uma outra aspa apareça, que seja diferente da previamente adicionada na stack, ela é adicionada ao topo.
    Uma aspa só é fechada quando corresponder à uma aspa no topo da stack.
    Caso tenha aspas abertas, trocamos o conteúdo dentro delas por *.

---
+++ sources/tokenization.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenization.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/01 15:08:56 by acesar-l          #+#    #+#             */
+/*   Updated: 2022/10/04 16:08:00 by acesar-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*tokenization(char *prompt)
+{
+	t_stack	**head;
+	char	*token;
+	int		i;
+
+	i = 0;
+	token = ft_strdup(prompt);
+	head = malloc(sizeof(t_stack **));
+	*head = NULL;
+	while (token[i])
+	{
+		if (token[i] == '\'' || token[i] == '"')
+		{
+			if (!(*head))
+				stack_push(head, stack_new(token[i]));
+			else if ((*head)->c == token[i])
+				stack_pop(head);
+			else if ((*head)->c != token[i])
+				stack_push(head, stack_new(token[i]));
+		}
+		if ((*head) && (token[i] != '\'') && (token[i] != '"'))
+			token[i] = '*';
+		i++;
+	}
+	clear_stack(head);
+	return (token);
+}
