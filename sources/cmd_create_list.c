d4e4f3226e8699616609642f49bcbe8fec4a3e43
Gabriel Souza <56346486+gawbsouza@users.noreply.github.com>
2022-11-28 12:45:37 +0000

    Aplicar norma

--- sources/cmd_create_list.c
+++ sources/cmd_create_list.c
@@ -6,25 +6,31 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 19:48:42 by gasouza           #+#    #+#             */
-/*   Updated: 2022/11/11 19:49:22 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/11/28 12:45:03 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_list *cmd_create_list(char *const *cmds)
+t_list	*cmd_create_list(char *const *cmds)
 {
 	t_list	*list_cmds;
 	t_list	*node;
 	t_cmd	*cmd;
-	
+
 	list_cmds = NULL;
-	while(cmds && *cmds)
+	while (cmds && *cmds)
 	{
-		cmd = cmd_parse_str(*cmds);		// TODO Pode ser NULL
-		node = ft_lstnew(cmd);			// TODO Pode ser NULL
-		
-		if(list_cmds == NULL)
+		cmd = cmd_parse_str(*cmds);
+		if (!cmd)
+			continue ;
+		node = ft_lstnew(cmd);
+		if (!node)
+		{
+			cmd_destroy(&cmd);
+			continue ;
+		}
+		if (list_cmds == NULL)
 			list_cmds = node;
 		else
 			ft_lstadd_back(&list_cmds, node);
